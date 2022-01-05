f5156374ceee11f28e88af6d8c74dd42575f3427
dittmar <dittmar.krall@matique.de>
2022-01-05 11:32:26 +0100

    'Models tests running again'

--- lib/mimoco.rb
+++ lib/mimoco.rb
@@ -10,13 +10,16 @@ module Minitest
       Controllers.run data, self
     end
 
+    class ValidMissingError < StandardError; end
+
     class Base
-      attr_accessor :minitest, :model
+      attr_accessor :minitest, :model, :model_hash
 
       def self.run(data, minitest)
         @minitest = minitest
         data.each do |model, hash|
           @model = model
+          @model_hash = hash
           hash.each do |key, value|
             send(key, value)
           end
@@ -40,6 +43,49 @@ module Minitest
       def self.invalids(arr)
         arr.each { |params| one_invalid(params) }
       end
+
+      def self.class_methods(methods)
+        cls = @model.methods(false).sort
+        cls.delete_if { |x| /^_/ =~ x }
+        cls.delete_if { |x| /^(after|before|find_by)_/ =~ x }
+        cls -= %i[column_headers attribute_type_decorations
+          attributes_to_define_after_schema_loads
+          default_scope_override defined_enums]
+        names = "[#{methods.sort.join(" ")}]"
+        msg = "#{@model.name} must have class_methods #{names}"
+        @minitest.assert_equal methods.sort.map(&:to_sym), cls, msg
+      end
+
+      # call class methods; don't check result
+      def self.call_class_methods(methods)
+        methods.each { |method|
+          msg = "#{msg} #{method} should return no nil"
+          @minitest.refute_nil @model.send(method), msg
+        }
+      end
+
+      def self.public_methods(methods)
+        cls = @model.public_instance_methods(false).sort
+        cls -= %i[autosave_associated_records_for_projects
+          validate_associated_records_for_projects]
+        names = "[#{methods.sort.join(" ")}]"
+        msg = "#{@model.name} must have public_methods #{names}"
+        @minitest.assert_equal methods.sort.map(&:to_sym), cls, msg
+      end
+
+      # call_public_methods; don't check result
+      def self.call_public_methods(methods)
+        model_params = @model_hash[:valid] || @model_hash[:valids]&.first
+        raise ValidMissingError unless model_params
+
+        methods.each { |method|
+          row, msg = @model.create(model_params)
+          msg = "#{msg} #{method} should return no nil"
+          @minitest.refute_nil row.send(method), msg
+        }
+      end
+
+      private
 
       def self.one_valid(params)
         row, msg = create_model(params)
--- test/models/models_test.rb
+++ test/models/models_test.rb
@@ -37,37 +37,42 @@ class ModelsTest < Minitest::Test
     check_models models
   end
 
-#  def test_class_methods
-#    models = {Order => {class_methods: %i[class_method]}}
-#    check_models models
-#  end
-#
-#  def test_call_class_methods
-#    models = {Order => {call_class_methods: %i[class_method]}}
-#    check_models models
-#  end
-#
-#  def test_public_methods
-#    models = {Order => {public_methods: %i[public_method]}}
-#    check_models models
-#  end
-#
-#  def test_call_public_methods
-#    models = {Order => {call_public_methods: %i[public_method]}}
-#    check_models models
-#  end
-#
-#  def test_some
-#    models = {
-#      Order => {
-#        valid: {name: "Name", qty: 123},
-#        invalid: {qty: :abc},
-#        class_methods: %i[class_method],
-#        call_class_methods: %i[class_method],
-#        public_methods: %i[public_method],
-#        call_public_methods: %i[public_method]
-#      }
-#    }
-#    check_models models
-#  end
+  def test_class_methods
+    models = {Order => {class_methods: %i[class_method]}}
+    check_models models
+  end
+
+  def test_call_class_methods
+    models = {Order => {call_class_methods: %i[class_method]}}
+    check_models models
+  end
+
+  def test_public_methods
+    models = {Order => {public_methods: %i[public_method]}}
+    check_models models
+  end
+
+  def test_call_public_methods
+    models = {
+      Order => {
+        valid: {name: "Name", qty: 123},
+        call_public_methods: %i[public_method]
+      }
+    }
+    check_models models
+  end
+
+  def test_several
+    models = {
+      Order => {
+        valid: {name: "Name", qty: 123},
+        invalid: {qty: :abc},
+        class_methods: %i[class_method],
+        call_class_methods: %i[class_method],
+        public_methods: %i[public_method],
+        call_public_methods: %i[public_method]
+      }
+    }
+    check_models models
+  end
 end
